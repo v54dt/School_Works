a117d048b1d7c44f9ad504bc85153f20694f6647
v54dt <vincent71497867@gmail.com>
2019-10-22 18:49:12 +0800

    Update sahw2-2.sh

--- sahw2-2.sh
+++ sahw2-2.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
-#dialog --menu "sys info" 15 40 4 "1" "CPU INFO" "2" "MEMORY INFO" "3" "NETWORK INFO" "4" "FILE BROWSER"
-
-#!/bin/bash
-OPTION=$(dialog --title "SYS INFO" --menu "" 15 60 4 \
-"1" "CPU INFO" \
-"2" "Memory INFO" \
-"3" "NETWORK INFO" \
-"4" "FILE BROWSER"  3>&1 1>&2 2>&3)
+OPTION=$(whiptail --title "Test Menu Dialog" --menu "Choose your option" 15 60 4 \
+"1" "Grilled Spicy Sausage" \
+"2" "Grilled Halloumi Cheese" \
+"3" "Charcoaled Chicken Wings" \
+"4" "Fried Aubergine"  3>&1 1>&2 2>&3)
  
 exitstatus=$?
-if [ $exitstatus = 1 ]; then
-    dialog --title "CPU INFO" --gauge "tt" 10 40 27
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $OPTION
 else
     echo "You chose Cancel."
 fi
