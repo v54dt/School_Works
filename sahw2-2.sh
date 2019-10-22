8158385c5e9789a97eb8b38ddd483791cdcefc56
v54dt <vincent71497867@gmail.com>
2019-10-22 18:25:00 +0800

    Update sahw2-2.sh

--- sahw2-2.sh
+++ sahw2-2.sh
@@ -1,43 +1,17 @@
 #!/bin/bash
 
-dialog --menu "sys info" 15 40 4 "1" "CPU INFO" "2" "MEMORY INFO" "3" "NETWORK INFO" "4" "FILE BROWSER"
+#dialog --menu "sys info" 15 40 4 "1" "CPU INFO" "2" "MEMORY INFO" "3" "NETWORK INFO" "4" "FILE BROWSER"
 
-
-
-# sys info
-#   1. CPU info  
-#   2.memory info
-#   3.network info
-#   4. file browser
-
-dialog --title "Backup" --msgbox "Time for backup \ of home directory. \
-Insert formatted 3-1/2\" floppy and press <Enter> \ to start backup or \
-<Esc> to cancel." 10 50
-# Return status of non-zero indicates cancel
-if [ "$?" != "0" ]; then
-  dialog --title "Backup" --msgbox "Backup was \ canceled at your
-  request." 10 50
+#!/bin/bash
+OPTION=$(dialog --title "Test Menu Dialog" --menu "Choose your option" 15 60 4 \
+"1" "Grilled Spicy Sausage" \
+"2" "Grilled Halloumi Cheese" \
+"3" "Charcoaled Chicken Wings" \
+"4" "Fried Aubergine"  3>&1 1>&2 2>&3)
+ 
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "Your chosen option:" $OPTION
 else
-  dialog --title "Backup" --infobox "Backup in \ process..." 10 50
-  cd ~
-  # Backup using tar; redirect any errors to a
-  # temporary file
-  # For multi-disk support, you can use the
-  # -M option to tar
-  tar -czf /dev/fd1 . >|/tmp/ERRORS$$ 2>&1
-  # zero status indicates backup was successful
-  if [ "$?" = "0" ]; then
-    dialog --title "Backup" --msgbox "Backup \
-completed successfully." 10 50
-    # Mark script with current date and time
-    touch ~/.backup
-  else
-    # Backup failed, display error log
-    dialog --title "Backup" --msgbox "Backup failed \ -- Press
-<Enter>
-    to see error log." 10 50
-    dialog --title "Error Log" --textbox /tmp/ERRORS$$ 22 72
-  fi
+    echo "You chose Cancel."
 fi
-
-clear
