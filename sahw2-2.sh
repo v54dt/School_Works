65b31516aab1ced4e5aaa20637f0c7ac0e8e0782
v54dt <vincent71497867@gmail.com>
2019-10-22 18:21:04 +0800

    Update sahw2-2.sh

--- sahw2-2.sh
+++ sahw2-2.sh
@@ -1,13 +1,20 @@
-
 #!/bin/bash
-# Backup all files under home directory to a single # floppy
-# Display message with option to cancel
+
+dialog --menu "sys info" 15 40 4 "1" "CPU INFO" "2" "MEMORY INFO" "3" "NETWORK INFO" "4" "FILE BROWSER"
+
+
+
+# sys info
+#   1. CPU info  
+#   2.memory info
+#   3.network info
+#   4. file browser
+
 dialog --title "Backup" --msgbox "Time for backup \ of home directory. \
 Insert formatted 3-1/2\" floppy and press <Enter> \ to start backup or \
 <Esc> to cancel." 10 50
 # Return status of non-zero indicates cancel
-if [ "$?" != "0" ]
-then
+if [ "$?" != "0" ]; then
   dialog --title "Backup" --msgbox "Backup was \ canceled at your
   request." 10 50
 else
@@ -19,8 +26,7 @@ else
   # -M option to tar
   tar -czf /dev/fd1 . >|/tmp/ERRORS$$ 2>&1
   # zero status indicates backup was successful
-  if [ "$?" = "0" ]
-    then
+  if [ "$?" = "0" ]; then
     dialog --title "Backup" --msgbox "Backup \
 completed successfully." 10 50
     # Mark script with current date and time
@@ -30,7 +36,7 @@ completed successfully." 10 50
     dialog --title "Backup" --msgbox "Backup failed \ -- Press
 <Enter>
     to see error log." 10 50
-   dialog --title "Error Log" --textbox /tmp/ERRORS$$ 22 72
+    dialog --title "Error Log" --textbox /tmp/ERRORS$$ 22 72
   fi
 fi
 
