a09aa1f29140a23419b2ccae1b7d98e277a6c1ad
v54dt <vincent71497867@gmail.com>
2019-10-22 19:42:45 +0800

    Update sahw2-2.sh

--- sahw2-2.sh
+++ sahw2-2.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-OPTION=$(whiptail --title "Test Menu Dialog" --menu "Choose your option" 15 60 4 \
-"1" "Grilled Spicy Sausage" \
-"2" "Grilled Halloumi Cheese" \
-"3" "Charcoaled Chicken Wings" \
-"4" "Fried Aubergine"  3>&1 1>&2 2>&3)
- 
+OPTION=$(
+  dialog --title "Test Menu Dialog" --menu "Choose your option" 15 60 4 \
+  "1" "Grilled Spicy Sausage" \
+  "2" "Grilled Halloumi Cheese" \
+  "3" "Charcoaled Chicken Wings" \
+  "4" "Fried Aubergine" 3>&1 1>&2 2>&3
+)
+
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "Your chosen option:" $OPTION
+  if [$OPTION = 1]; then
+    #dialog --title "CPU INFO" --gauge 5 30 27
+    dialog --msgbox "${sysctl hw.model | awk -F":" '{print("CPU Model:","\t",$2)}'}"
+    sysctl hw.model | awk -F":" '{print("CPU Model:","\t",$2)}'
+  fi
+
 else
-    echo "You chose Cancel."
+  echo "You chose Cancel."
 fi
