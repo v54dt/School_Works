9d8311ed80f0a2b8da63019897cfeb9b7e781d4c
v54dt <vincent71497867@gmail.com>
2019-10-22 20:02:53 +0800

    Update sahw2-2.sh

--- sahw2-2.sh
+++ sahw2-2.sh
@@ -11,10 +11,17 @@ exitstatus=$?
 if [ $exitstatus = 0 ]; then
   if [$OPTION = 1]; then
     #dialog --title "CPU INFO" --gauge 5 30 27
-    dialog --msgbox "${sysctl hw.model | awk -F":" '{print("CPU Model:","\t",$2)}'}"
-    sysctl hw.model | awk -F":" '{print("CPU Model:","\t",$2)}'
+    #dialog --msgbox "${sysctl hw.model | awk -F":" '{print("CPU Model:","\t",$2)}'}"
+    result_model=$(sysctl hw.model | awk -F":" '{print("CPU Model:","\t",$2)}')
+    result_machine=$(sysctl hw.machine | awk -F":" '{print("CPU Machine:","\t",$2)}')
+    result_ncpu=$(sysctl hw.ncpu | awk -F":" '{print("CPU Core:","\t",$2)}')
+    dialog --msgbox "CPU INFO \ ${result_model} \ ${result_machine} \ ${result_ncpu}" 15 60
+
+
+
   fi
 
 else
+
   echo "You chose Cancel."
 fi
